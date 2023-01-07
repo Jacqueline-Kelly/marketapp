b7410510ad34322e018208d89b21aaeac8e8d9d3
Jacqueline-Kelly <jacqueline.kelly.2016@gmail.com>
2023-01-07 09:58:36 +0200

    template and routes to shop page

---
+++ first-app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const dirRoot = require('./util/path')
+
+const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(dirRoot, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+})
+
+app.listen(3000)
